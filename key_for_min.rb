d0397c799e73c08be111b7bb7da9cf4dc2dab342
Jacob Marthaller <marthaller.jr@gmail.com>
2020-11-11 20:59:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,18 +1,58 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
+# # prereqs: iterators, hashes, conditional logic
+# # Given a hash with numeric values, return the key for the smallest value
+# require 'pry'
 
-def key_for_min_value(name_hash)
-  lowest_key = nil
+
+
+# def key_for_min_value(name_hash)
+#   lowest_key = nil
+#   lowest_value = nil
+#   if name_hash.length == 0
+#     nil
+#   else     
+#     name_hash.each do |key, value|
+#       if  lowest_value == nil || value < lowest_value
+#         lowest_value = value
+#         lowest_key = key
+#       end
+#     end
+#     lowest_key
+#   end
+# end
+
+
+require 'pry'
+
+
+def key_for_min_value(hash)
   lowest_value = nil
-  if name_hash.length == 0
-    return nil
-  else 
-    name_hash.each do |key, value|
-      if lowest_value == nil || value < lowest_value
-        lowest_value = value
-        lowest_key = key
-      end
+  lowest_key = nil
+  hash.each do |item, price|
+    if lowest_value == nil || price < lowest_value
+      lowest_value = price
+      lowest_key = item
     end
+    #binding.pry
   end
-  return lowest_key
+ # binding.pry
+  lowest_key
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
